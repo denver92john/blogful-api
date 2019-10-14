da3cb51c6c6cee6ae2a6585f2535566e743ef104
Burl Adkins <adkins1393@gmail.com>
2019-10-14 16:09:52 -0400

    edited DB_URL occurrences to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin:miff@localhost/blogful'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:miff@localhost/blogful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:miff@localhost/blogful-test'
 }
